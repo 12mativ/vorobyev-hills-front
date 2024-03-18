d97fd41390bfce494db8ff7f61fafba263d82508
A1Fizzy <artemarhunin@gmail.com>
2024-03-18 18:34:43 +0400

    button wrap

--- app/flights/page.tsx
+++ app/flights/page.tsx
@@ -40,12 +40,20 @@ const Page = () => {
           startCity: "Москва",
           endCity: "Владивосток",
         },
+        {
+          id: 4,
+          airplaneName: "Суперджет4",
+          startDate: "22.11.2024",
+          endDate: "02.12.2024",
+          startCity: "Москва",
+          endCity: "Владивосток",
+        },
       ])
     );
   }, []);
 
   return (
-    <div>
+    <div className="p-3">
       <button
         onClick={() => router.push('/')}
         className="flex text-white items-center gap-x-2 group mb-3 self-start text-lg rounded-lg font-semibold"
@@ -54,11 +62,11 @@ const Page = () => {
         <IoIosArrowBack size={30} />
       </button>
 
-      <div className="flex flex-row justify-center items-center overflow-y-auto h-screen">
+      <div className="flex flex-row flex-wrap justify-start items-start overflow-y-auto h-full w-full">
         {flights.map((flight) => (
           <button
             key={flight.id}
-            className="bg-sky-600 rounded-lg p-3 md:p-6 gap-y-6 flex flex-col w-[90%] md:w-[80%] lg:w-[55%] items-center m-2 h-60 transition hover:bg-sky-300"
+            className="bg-sky-600 rounded-lg p-3 md:p-6 gap-y-6 flex flex-col w-[90%] md:w-[49%] lg:w-[32%] items-center m-2 h-60 transition hover:bg-sky-300"
           >
             <p className="text-2xl font-bold text-white text-center">{flight.airplaneName}</p>
             <p className="text-xl font-semibold text-white text-center mt-auto">{flight.startDate} - {flight.endDate}</p>
