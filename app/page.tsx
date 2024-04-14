d5434b959592a805a0712624d4fce46ae9d04e21
12mativ <ivan_m146@mail.ru>
2024-04-14 10:20:32 +0400

    remove bug in index page

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="flex flex-col justify-center items-center overflow-y-auto h-full p-2">
+    <div className="flex flex-col gap-y-2 justify-center items-center overflow-y-auto h-full p-2">
       <div className="flex flex-col text-center p-4 rounded-lg bg-white min-w-[100%] md:min-w-[50%] shadow-lg">
         <h1 className="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-6xl font-black">Команда SHA-256</h1>
       </div>
 
-      <p className="animate-typing overflow-hidden whitespace-nowrap text-white text-3xl mt-5 text-center">Кейс</p>
-      <p className="text-white text-3xl underline text-center">Семантическая классификация документов</p>
+      <p className="text-white text-xl md:text-3xl mt-5 text-center">Кейс</p>
+      <p className="text-white text-xl md:text-3xl underline text-center">Семантическая классификация документов</p>
 
       <Link 
         href="/upload-file" 
